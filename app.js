0cf5847a68584d401c18cc39ab9d3e144c920901
Nikola Kosich <n.kosich@yandex.ru>
2023-06-08 01:57:22 +0300

    idk

--- app.js
+++ app.js
@@ -3,52 +3,27 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
-
-const {
-  notFoundError,
-} = require('./errors/errors');
-
-const {
-  createUser,
-  login,
-} = require('./controllers/users');
-
-const {
-  validateSignup,
-  validateSignin,
-} = require('./middlewares/validate');
 
 const router = require('./routes');
 
 const app = express();
 
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
 mongoose.connect(
-  'mongodb://127.0.0.1:27017/mestodb',
+  DB_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
 );
 
-const { PORT = 3000 } = process.env;
-
 app.use(helmet());
 
 app.use(bodyParser.json());
 
-app.post('/signup', validateSignup, createUser);
-
-app.post('/signin', validateSignin, login);
-
-app.use(auth);
-
 app.use(router);
-
-app.use('*', (req, res) => {
-  res.status(notFoundError).send({ message: 'Такой страницы нет' });
-});
 
 app.use(errors());
 app.use(errorHandler);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,39 @@
+const express = require('express');
+
 const router = require('express').Router();
+
+const auth = require('../middlewares/auth');
 
 const userRouter = require('./users');
 
 const cardRouter = require('./cards');
 
+const NotFoundError = require('../errors/NotFoundError');
+
+const routes = express();
+
+const {
+  createUser,
+  login,
+} = require('../controllers/users');
+
+const {
+  validateSignup,
+  validateSignin,
+} = require('../middlewares/validate');
+
+router.post('/signup', validateSignup, createUser);
+
+router.post('/signin', validateSignin, login);
+
+routes.use(auth);
+
 router.use(userRouter);
 
 router.use(cardRouter);
+
+router.use('*', (req, res, next) => {
+  next(new NotFoundError('Такой страницы не существует'));
+});
 
 module.exports = router;
