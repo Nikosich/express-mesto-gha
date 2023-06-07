0cbb064952b59337fc7bf9a595a8d4b43493e128
Nikola Kosich <n.kosich@yandex.ru>
2023-06-08 01:52:40 +0300

    fast-test

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,11 @@
-const express = require('express');
-
 const router = require('express').Router();
-
-const auth = require('../middlewares/auth');
 
 const userRouter = require('./users');
 
 const cardRouter = require('./cards');
 
-const NotFoundError = require('../errors/NotFoundError');
+router.use(userRouter);
 
-router.all('*', express.json());
-
-const {
-  createUser,
-  login,
-} = require('../controllers/users');
-
-const {
-  validateSignup,
-  validateSignin,
-} = require('../middlewares/validate');
-
-router.post('/signup', validateSignup, createUser);
-
-router.post('/signin', validateSignin, login);
-
-router.use(userRouter, auth);
-
-router.use(cardRouter, auth);
-
-router.use('*', (req, res, next) => {
-  next(new NotFoundError('Такой страницы не существует'));
-});
+router.use(cardRouter);
 
 module.exports = router;
