06075503e7aea5886eb5462974b0c760b52ffa85
Nikola <105383731+Nikosich@users.noreply.github.com>
2023-06-13 05:29:45 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,24 +1,28 @@
 const allowedCors = [
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
   'http://mesto.nksch.nomoredomains.rocks',
   'https://mesto.nksch.nomoredomains.rocks',
-  'http://localhost:3000',
-  'https://localhost:3000',
+  'http://62.84.113.201:3000',
 ];
-
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    res.header('Access-Control-Allow-Credentials', true);
+    return res.end();
   }
-  next();
+
+  return next();
 };
