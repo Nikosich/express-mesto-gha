3204c2e73b5d4a7f215cb85a98194f9579d76e44
Nikola <105383731+Nikosich@users.noreply.github.com>
2023-06-13 05:15:03 +0300

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,30 +1,10 @@
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'http://mesto.nksch.nomoredomains.rocks',
-  'https://mesto.nksch.nomoredomains.rocks',
-  'http://62.84.113.201:3000',
-];
-
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     return res.end();
   }
-
-  return next();
+  next();
 };
