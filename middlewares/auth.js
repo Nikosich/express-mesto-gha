fe0afa952f9890b9e8a16ab8068c399b9db25645
Nikola <105383731+Nikosich@users.noreply.github.com>
2023-06-14 15:19:08 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,4 @@
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 
@@ -7,13 +7,13 @@ module.exports = (req, res, next) => {
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthorizationError('Не авторизованный пользователь');
   }
+  
+  const token = authorization.replace('Bearer ', '');
 
   let payload;
 
-  const token = authorization.replace('Bearer ', '');
-
   try {
-    payload = jwt.verify(token, JWT_SECRET : 'secretKey');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'secretKey');
   } catch (err) {
     return next(new AuthorizationError('Не авторизованный пользователь'));
   }
