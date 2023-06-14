cec656137e5ac93950b7d1f7c05bc931ab63a1da
Nikola <105383731+Nikosich@users.noreply.github.com>
2023-06-14 16:36:39 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -22,8 +22,8 @@ const login = (req, res, next) => {
           if (!isValidPassword) {
             return next(new AuthorizationError('Неверные данные'));
           }
-          const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secretKey', { expiresIn: '7d' });
-          return res.status(200).send({ token });
+          const jwt = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secretKey', { expiresIn: '7d' });
+          return res.status(200).send({ jwt });
         });
     })
     .catch(next);
