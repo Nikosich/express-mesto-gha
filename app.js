6ee4bb39f342b5192e477c2bdb1bbe051fa524ab
Nikola <105383731+Nikosich@users.noreply.github.com>
2023-06-14 14:54:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
+console.log(require('dotenv').config());
+
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 mongoose.connect(
